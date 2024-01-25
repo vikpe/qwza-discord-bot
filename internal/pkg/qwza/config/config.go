96b9ba9501c1b4f5500f4f68e8306845a9529357
Viktor Persson <viktor.persson@arcsin.se>
2024-01-25 17:26:02 +0100

    Add config.

---
+++ internal/pkg/qwza/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"github.com/goccy/go-json"
+	"os"
+)
+
+type Config struct {
+	Monitor Monitor `json:"monitor"`
+}
+
+type Monitor struct {
+	Servers []string `json:"servers"`
+	Channel string   `json:"channel"`
+}
+
+func FromFile(path string) (*Config, error) {
+	bytes, err := os.ReadFile(path)
+
+	if err != nil {
+		return &Config{}, err
+	}
+
+	var config Config
+	err = json.Unmarshal(bytes, &config)
+	return &config, nil
+}
