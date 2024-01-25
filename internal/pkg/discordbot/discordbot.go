2609c19f2b56e293111a5d326482b63d803dd11c
Viktor Persson <viktor.persson@arcsin.se>
2024-01-25 17:25:20 +0100

    Add generic discord bot.

---
+++ internal/pkg/discordbot/discordbot.go
@@ -0,0 +1,53 @@
+package discordbot
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type Bot struct {
+	session *discordgo.Session
+	guildID string
+	OnReady func(s *discordgo.Session)
+}
+
+func New(token string, guildId string) (*Bot, error) {
+	session, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		return &Bot{}, err
+	}
+
+	return &Bot{
+		session: session,
+		guildID: guildId,
+		OnReady: func(s *discordgo.Session) {
+			log.Println(fmt.Sprintf("%s is ready", s.State.User.Username))
+		},
+	}, nil
+}
+
+func (b *Bot) Start() {
+	log.Println("Start()")
+
+	b.session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		b.OnReady(s)
+	})
+
+	err := b.session.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer b.session.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	log.Println("Gracefully shutting down.")
+}
